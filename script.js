0b44325b90116624e42cc5b179662ac8fbea8a19
Manojkumar K <manojkannan189@gmail.com>
2025-07-25 15:25:44 +0530

    first comitment

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("home").addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+
+  document.getElementById("about").addEventListener("click", () => {
+    window.location.href = "about.html";
+  });
+
+  document.getElementById("services").addEventListener("click", () => {
+    window.location.href = "services.html";
+  });
+
+  document.getElementById("skills").addEventListener("click", () => {
+    window.location.href = "skills.html";
+  });
+
+  document.getElementById("contact").addEventListener("click", () => {
+    window.location.href = "contact.html";
+  });
+});
